52ae7f23f7b0e78136d20329c522c429c01d8f42
Kirill Lagutin <lagutin.k@mail.ru>
2022-02-15 01:28:48 +0300

    Init

--- Students.App/MainWindow.xaml.cs
+++ Students.App/MainWindow.xaml.cs
@@ -1,28 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Students.Model;
 
 namespace Students.App
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public Account Account { get; set; }
+        public MainWindow(Account account)
         {
             InitializeComponent();
+
+            var role = account.TabAccountRoles.First().Role;
+            switch (role.Name)
+            {
+                case "student":
+                    SetTabItemSelected(TabItem_Student, TabItem_Admin, TabItem_Teacher);
+                    break;
+                case "teacher":
+                    SetTabItemSelected(TabItem_Teacher, TabItem_Admin, TabItem_Student);
+                    break;
+                case "admin":
+                    SetTabItemSelected(TabItem_Admin, TabItem_Teacher, TabItem_Student);
+                    break;
+            }
+
+        }
+
+        private void SetTabItemSelected(TabItem active, params TabItem[] notActives)
+        {
+            TabControl_User.SelectedItem = active;
+            foreach (var item in notActives)
+            {
+                item.IsEnabled = false;
+            }
         }
     }
 }
